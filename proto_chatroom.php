ac241de76c4756f914034cfff8ca36a961d61a7e
DSoliz <zilosgodi@gmail.com>
2016-04-08 13:33:14 -0500

    Auto Refresh and actual chat capability.

--- proto_chatroom.php
+++ proto_chatroom.php
@@ -1,45 +1,40 @@
 <html>
   <head>
-    <script type="text/javascript"
-			src="Post.js">
-	</script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js">
 	</script>
 
-    <div style="width:300px; height: 300px;background-color:black; color:white; padding:10px; overflow-y: scroll; overflow-x:hidden;  word-wrap: break-word;">
-
-    <p>Welcome</p>
+    <div id="chatBox" style="width:300px; height: 300px;background-color:black; color:white; padding:10px; overflow-y: scroll; overflow-x:hidden;  word-wrap: break-word;">
 	
 	<?php
-
-	$pass_value = $_POST['message'];
-	$msg = $pass_value;
-	$formated_msg = "<p>".$msg."</p>";
-	
+	//Here you catch message as sent by the submit form
+	$msg = $_POST['message'];
+	//Formatting the message
+	if($msg != "" && $msg != NULL){
+		$formated_msg = "<p>".$msg."</p>";
+		//Opening the log
+		$myfile = fopen("roomlog.txt", "a+");
+		//writing the message to the log
+		fwrite($myfile, $formated_msg);
+	}
+	//Opening file again and displaying its contents
 	$myfile = fopen("roomlog.txt", "a+");
-	fwrite($myfile, $formated_msg);
-	$myfile = fopen("roomlog.txt", "a+");
-	
 	echo fread($myfile,filesize("roomlog.txt"));
-
 	fclose($myfile);
-	
-	/*
-	$url1=$_SERVER['REQUEST_URI'];
-	header("Refresh: 1; URL=$url1");
-	*/
 	?>
 
     </div>
+	
 	<script>
-	$("div").scrollTop(100000000000);
+	//scrolling to the bottom
+		$("div").scrollTop(100000000000);
+		setInterval(function(){ 
+			$("#chatBox").load("chatContent.php");
+			}, 250);
 	</script>
 	
 	<br>
 	<textarea name="message" rows="2" cols="40" id="textInput" form="myform" style="resize: none;overflow:auto;"></textarea>
-	
-		<br>
-		
+	<br>
 		<form action="proto_chatroom.php" method="POST" id="myform">
 			<input type="submit">
 		</form>
@@ -47,7 +42,5 @@
 		<form action="ChatRemove.php">
 			<input type="submit" value="Leave Chat">
 		</form>
-		
   </head>
-  <body></body>
 </html>
